2ef4abda9d93c27902398f812b40235382f7fdff
Atul Kumar <aayanchauhan.90412@gmail.com>
2021-11-17 20:34:54 +0530

    adding files

---
+++ models/Menu.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const dbConnection = require('../database/db');
+dbConnection.dbInit()
+
+// w27d4userSignUpData
+// XmGZxQUFihfKtaPI
+
+const MenuSchema = new mongoose.Schema({
+
+    name: String,
+    price: Number,
+
+})
+
+const MenuModel = mongoose.model('Menu', MenuSchema)
+
+module.exports = UserModel
+
